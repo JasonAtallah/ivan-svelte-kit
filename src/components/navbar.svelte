486c2126acad78a006d91de002b41d8f95ea0a9c
Jason Atallah <jasonatallah@gmail.com>
2023-02-14 21:59:09 -0800

    navbar now takes you home

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,1 +1,7 @@
-<nav class="bg-white shadow-sm p-4 w-screen font-bold text-xl">IVAN</nav>
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
+<nav class="bg-white shadow-sm p-4 w-screen">
+	<button on:click={() => goto('/')} class="font-bold text-xl">IVAN</button>
+</nav>
